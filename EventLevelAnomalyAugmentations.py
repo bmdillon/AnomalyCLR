139256aa1950c41312bd19f87aa68203ab85b1aa
Luigi Favaro <favaro@thphys.uni-heidelberg.de>
2024-06-18 18:21:46 +0200

    bugfix and update augmentations

--- EventLevelAnomalyAugmentations.py
+++ EventLevelAnomalyAugmentations.py
@@ -6,7 +6,8 @@ import numpy as np
 #################################################
 #################################################
 
-def rotate_events(event, scaler=None): 
+def rotate_events(event, scaler=None):
+    event_rot = event.copy() # changed
     phi = event[:, 2, :]
     particle = event[:, 3] + event[:, 4] + event[:, 5] + event[:, 6]
     rot_angle = np.random.rand(event.shape[0]) *2*np.pi
@@ -23,8 +24,8 @@ def rotate_events(event, scaler=None):
     else: 
         phi = np.where(phi>np.pi, phi - 2*np.pi, phi)
         phi = np.where(phi<-np.pi, phi+2*np.pi, phi)
-    event[:, 2, :] = phi 
-    return event
+    event_rot[:, 2, :] = phi 
+    return event_rot
 
 def energy_smear_jets(batch, scaler_pt, strength=1.0):
     batch_esmeared = batch.copy()
@@ -159,6 +160,63 @@ def collinear_fill_e_mu(batch):
     batch_filled[:, 5, 5:9] += np.flip(mask_split*batch_filled[:, 5, 5:9], axis=1)
     return batch_filled
 
+def collinear_fill_e_mu_v2(batch, scaler_pt):
+    test_batch_c = batch.copy()
+    # ELECTRONS
+    pt_led = 23/scaler_pt
+    pt_min = 3/scaler_pt
+    n_constit = 4
+    
+    mask_split = test_batch_c[:, 4, 1:5] != 0 # loc og n_non_zero
+    n_nonzero = mask_split.sum(-1)
+    n_allowed = np.maximum(0, ((test_batch_c[:,0, 1:5].sum(-1)-pt_led)/pt_min) - n_nonzero)
+    n_adds = np.random.randint(0, high=np.trunc(n_allowed)+1)
+    n_adds = np.minimum(n_adds, mask_split.sum(-1), n_constit-mask_split.sum(-1))
+    pt_avail = np.maximum(0, test_batch_c[:,0, 1:5].sum(-1) - (pt_led-pt_min) - pt_min*(n_adds+n_nonzero))
+
+    test_batch_c[:, 4, 1:5] = mask_split +  np.flip(mask_split, axis=1)
+
+    row_indices = np.arange(len(test_batch_c)).reshape(-1,1)
+    col_indices = np.arange(test_batch_c[:,0,1:5].shape[-1])
+    bool_mask = np.flip((col_indices < n_adds[:, None]), axis=-1)
+    mask_split |= bool_mask
+
+    sample = np.random.gamma(test_batch_c[:, 4, 1:5].astype(int)*mask_split,1)
+    sample_sum = np.sum(sample, axis=1, keepdims=True)
+    sample = np.divide(sample, sample_sum, out=np.zeros_like(sample), where=sample_sum!=0)
+
+    test_batch_c[:,0,1:5] = ((test_batch_c[:, 4, 1:5]*pt_avail.reshape(-1,1)*sample + pt_min)*mask_split)
+    test_batch_c[:,0,1] += np.maximum(0, (pt_led - pt_min))*mask_split[:,0]
+    test_batch_c[:, 1, 1:5] += np.flip(mask_split*test_batch_c[:, 1, 1:5], axis=1)
+    test_batch_c[:, 2, 1:5] += np.flip(mask_split*test_batch_c[:, 2, 1:5], axis=1)
+    test_batch_c[:, 4, 1:5] *= mask_split
+
+    #MUONS
+    mask_split = test_batch_c[:, 5, 5:9] != 0 # loc og n_non_zero
+    n_nonzero = mask_split.sum(-1)
+    n_allowed = np.maximum(0, ((test_batch_c[:,0, 5:9].sum(-1)-pt_led)/pt_min) - n_nonzero)
+    n_adds = np.random.randint(0, high=np.trunc(n_allowed)+1)
+    n_adds = np.minimum(n_adds, mask_split.sum(-1), n_constit-mask_split.sum(-1))
+    pt_avail = np.maximum(0, test_batch_c[:,0, 5:9].sum(-1) - (pt_led-pt_min) - pt_min*(n_adds+n_nonzero))
+
+    test_batch_c[:, 5, 5:9] = mask_split +  np.flip(mask_split, axis=1)
+
+    row_indices = np.arange(len(test_batch_c)).reshape(-1,1)
+    col_indices = np.arange(test_batch_c[:,0,5:9].shape[-1])
+    bool_mask = np.flip((col_indices < n_adds[:, None]), axis=-1)
+    mask_split |= bool_mask
+
+    sample = np.random.gamma(test_batch_c[:, 5, 5:9].astype(int)*mask_split,1)
+    sample_sum = np.sum(sample, axis=1, keepdims=True)
+    sample = np.divide(sample, sample_sum, out=np.zeros_like(sample), where=sample_sum!=0)
+
+    test_batch_c[:,0,5:9] = ((test_batch_c[:, 5, 5:9]*pt_avail.reshape(-1,1)*sample + pt_min)*mask_split)
+    test_batch_c[:,0,5] += np.maximum(0, (pt_led - pt_min))*mask_split[:,0]
+    test_batch_c[:, 1, 5:9] += np.flip(mask_split*test_batch_c[:, 1, 5:9], axis=1)
+    test_batch_c[:, 2, 5:9] += np.flip(mask_split*test_batch_c[:, 2, 5:9], axis=1)
+    test_batch_c[:, 5, 5:9] *= mask_split
+    return test_batch_c
+
 def collinear_fill_jets (batch):
     batch_filled = batch.copy()
     n_constit = 10
@@ -178,7 +236,37 @@ def collinear_fill_jets (batch):
     batch_filled[:, 6, 9:] += np.flip(mask_split*batch_filled[:, 6, 9:], axis=1)
     return batch_filled
 
-def add_objects (batch):
+def collinear_fill_jets_v2 (batch, scaler_pt):
+    test_batch_c = batch.copy()
+    pt_led = 0
+    pt_min = 15/scaler_pt
+    n_constit = 10
+    mask_split = test_batch_c[:, 6, 9:] != 0 # loc og n_non_zero
+    n_nonzero = mask_split.sum(-1)
+    n_allowed = np.maximum(0, ((test_batch_c[:,0, 9:].sum(-1)-pt_led)/pt_min) - n_nonzero)
+    n_adds = np.random.randint(0, high=np.trunc(n_allowed)+1)
+    n_adds = np.minimum(n_adds, mask_split.sum(-1), n_constit-mask_split.sum(-1))
+    pt_avail = np.maximum(0, test_batch_c[:,0, 9:].sum(-1) - (pt_led - pt_min) - pt_min*(n_adds+n_nonzero))
+
+    test_batch_c[:, 6, 9:] = mask_split +  np.flip(mask_split, axis=1)
+
+    row_indices = np.arange(len(test_batch_c)).reshape(-1,1)
+    col_indices = np.arange(test_batch_c[:,0,9:].shape[-1])
+    bool_mask = np.flip((col_indices < n_adds[:, None]), axis=-1)
+    mask_split |= bool_mask
+
+    sample = np.random.gamma(test_batch_c[:, 6, 9:].astype(int)*mask_split,1)
+    sample_sum = np.sum(sample, axis=1, keepdims=True)
+    sample = np.divide(sample, sample_sum, out=np.zeros_like(sample), where=sample_sum!=0)
+
+    test_batch_c[:,0,9:] = ((test_batch_c[:, 6, 9:]*pt_avail.reshape(-1,1)*sample + pt_min)*mask_split)
+    test_batch_c[:,0,9] += np.maximum(0, (pt_led - pt_min))
+    test_batch_c[:, 1, 9:] += np.flip(mask_split*test_batch_c[:, 1, 9:], axis=1)
+    test_batch_c[:, 2, 9:] += np.flip(mask_split*test_batch_c[:, 2, 9:], axis=1)
+    test_batch_c[:, 6, 9:] *= mask_split
+    return test_batch_c
+
+def add_objects (batch, scaler_pt=1., scale_angle=False):
     batch_filled = batch.copy()
     n_els = 4
     n_mus = 4
@@ -196,7 +284,7 @@ def add_objects (batch):
         el_phis = np.expand_dims( 2*np.pi * ( np.random.rand(n_new_els[n]) - 0.5 ), axis=1 )
         el_etas = np.expand_dims( 2*3 * ( np.random.rand(n_new_els[n]) - 0.5 ), axis=1 )
         el_one_hot = np.concatenate( [np.zeros(shape=(n_new_els[n],1)), np.ones(shape=(n_new_els[n],1)), np.zeros(shape=(n_new_els[n],1)), np.zeros(shape=(n_new_els[n],1))], axis=1 ) 
-        els = np.concatenate( [el_pts, el_phis, el_etas, el_one_hot], axis=1 )
+        els = np.concatenate( [el_pts, el_etas, el_phis, el_one_hot], axis=1 )
         el_start = 1 + n_nonzero_els[n]
         el_end = 1 + n_nonzero_els[n] + n_new_els[n]
         batch_filled[n,:,el_start:el_end] = np.transpose( els )
@@ -205,7 +293,7 @@ def add_objects (batch):
         mu_phis = np.expand_dims( 2*np.pi * ( np.random.rand(n_new_mus[n]) - 0.5 ), axis=1 )
         mu_etas = np.expand_dims( 2*2.1 * ( np.random.rand(n_new_mus[n]) - 0.5 ), axis=1 )
         mu_one_hot = np.concatenate( [np.zeros(shape=(n_new_mus[n],1)), np.zeros(shape=(n_new_mus[n],1)), np.ones(shape=(n_new_mus[n],1)), np.zeros(shape=(n_new_mus[n],1))], axis=1 ) 
-        mus = np.concatenate( [mu_pts, mu_phis, mu_etas, mu_one_hot], axis=1 )
+        mus = np.concatenate( [mu_pts, mu_etas, mu_phis, mu_one_hot], axis=1 )
         mu_start = 5 + n_nonzero_mus[n]
         mu_end = 5 + n_nonzero_mus[n] + n_new_mus[n]
         batch_filled[n,:,mu_start:mu_end] = np.transpose( mus )
@@ -214,7 +302,7 @@ def add_objects (batch):
         jet_phis = np.expand_dims( 2*np.pi * ( np.random.rand(n_new_jets[n]) - 0.5 ), axis=1 )
         jet_etas = np.expand_dims( 2*4 * ( np.random.rand(n_new_jets[n]) - 0.5 ), axis=1 )
         jet_one_hot = np.concatenate( [np.zeros(shape=(n_new_jets[n],1)), np.zeros(shape=(n_new_jets[n],1)), np.zeros(shape=(n_new_jets[n],1)), np.ones(shape=(n_new_jets[n],1))], axis=1 ) 
-        jets = np.concatenate( [jet_pts, jet_phis, jet_etas, jet_one_hot], axis=1 )
+        jets = np.concatenate( [jet_pts, jet_etas, jet_phis, jet_one_hot], axis=1 )
         jet_start = 9 + n_nonzero_jets[n]
         jet_end = 9 + n_nonzero_jets[n] + n_new_jets[n]
         batch_filled[n,:,jet_start:jet_end] = np.transpose( jets )
@@ -223,18 +311,29 @@ def add_objects (batch):
         old_met_phi = batch_filled[n,2,0]
         old_met = np.array( [ old_met_pt * np.sin(old_met_phi), old_met_pt * np.cos(old_met_phi) ] )
         new_obj = np.concatenate( [ els[:,0:3], mus[:,0:3], jets[:,0:3] ], axis=0 )
-        new_met = old_met + np.array( [ new_obj[:,0] * np.sin(new_obj[:,2]), new_obj[:,0] * np.cos(new_obj[:,2]) ] ).sum(axis=-1)
+        new_met = old_met - np.array( [ new_obj[:,0] * np.sin(new_obj[:,2]), new_obj[:,0] * np.cos(new_obj[:,2]) ] ).sum(axis=-1)
         new_met_pt = np.sqrt( new_met[0]**2 + new_met[1]**2 )
-        new_met_phi = np.arcsin( new_met[0]/new_met_pt )
-        batch_filled[n,0,0] = new_met_pt
-        batch_filled[n,2,0] = new_met_phi
+        if new_met[1]<0. and new_met[0]>0.:
+            new_met_phi =  np.pi - np.arcsin( new_met[0]/new_met_pt )
+        elif new_met[1]<0. and new_met[0]<0.:
+            new_met_phi = -np.pi - np.arcsin( new_met[0]/new_met_pt )
+        else:
+            new_met_phi = np.arcsin( new_met[0]/new_met_pt )
+        batch_filled[n,0,0] = new_met_pt / scaler_pt
+        if scale_angle:
+            batch_filled[n,2,0] = new_met_phi / np.pi
     return batch_filled
 
+def add_objects_wsmear(batch, scaler_pt, scale_angle, etaphi_smear_strength):
+    batch_filled = batch.copy()
+    batch_filled = add_objects( batch_filled, scaler_pt, scale_angle )
+    batch_filled = etaphi_smear_events( batch_filled, scaler_pt, scale_angle, strength=etaphi_smear_strength )
+    return batch_filled
 
 def add_objects_constptmet( batch, scaler_pt, scale_angle, etaphi_smear_strength ):
     batch_filled = batch.copy()
-    batch_filled = collinear_fill_jets( batch_filled )
-    batch_filled = collinear_fill_e_mu( batch_filled )
+    batch_filled = collinear_fill_jets_v2( batch_filled, scaler_pt)
+    batch_filled = collinear_fill_e_mu_v2( batch_filled, scaler_pt )
     batch_filled = etaphi_smear_events( batch_filled, scaler_pt, scale_angle, strength=etaphi_smear_strength )
     return batch_filled
 
@@ -261,8 +360,7 @@ def neg_augs( batch, scaler_pt, scale_angle, etaphi_smear_strength, addobj=True,
     for j in range( n_augs ):
         aug = aug_list[j]
         n = rand_opts[j]
-        if aug=="ao":       batch_aug[ np.where(rands==n) ] = add_objects( batch_aug[ np.where(rands==n) ] )
+        if aug=="ao":       batch_aug[ np.where(rands==n) ] = add_objects_wsmear( batch_aug[ np.where(rands==n) ], scaler_pt, scale_angle, etaphi_smear_strength)
         if aug=="aowcpm":   batch_aug[ np.where(rands==n) ] = add_objects_constptmet( batch_aug[ np.where(rands==n) ], scaler_pt, scale_angle, etaphi_smear_strength )
         if aug=="spm":      batch_aug[ np.where(rands==n) ] = shift_met_or_pt( batch_aug[ np.where(rands==n) ] )
     return batch_aug
-
